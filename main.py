8ece903aa0920b4a5f308eedf8e8b40088fff3aa
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2021-03-07 21:00:36 +0100

    lista laureatów z większą liczbą nagród niż 1

--- main.py
+++ main.py
@@ -22,9 +22,24 @@ for laureate in data_nobel['laureates']:
             # prize_economy.append(laureate['surname'] + ' ' + laureate['firstname']) #sortowanie bedzie według nazwiska
             break
 prize_economy = sorted(prize_economy)
-print(prize_economy)
+# print(prize_economy)
 
+#c) wypisz osoby które otrzymały nagordę wiecej niż raz wraz z ilością nagród, kolejnośc w zależności od wielokrotności nagrodzenia.
+prize_more_than_one = []
 
+class Laureate:
+    def __init__(self, firstname, surname, prizes):
+        self.firstname = firstname
+        self.surname = surname
+        self.prizes = prizes
 
+    def __repr__(self):
+        return self.firstname + " " + self.surname + " " + str(self.prizes)
 
+for laureate in data_nobel['laureates']:
+    if len(laureate['prizes']) > 1:
+        prize_more_than_one.append(Laureate(laureate.get('firstname',''), laureate.get('surname',''), len(laureate['prizes'])))
+
+prize_more_than_one = sorted(prize_more_than_one, key=lambda x: x.prizes, reverse=True)
+print(prize_more_than_one)
 
