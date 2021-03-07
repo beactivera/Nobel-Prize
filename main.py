54e346b4f2d11e405f0a6ba7306ec01e585f251f
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2021-03-07 19:48:53 +0100

    nagrodzeni w dziedzinie ekonomii

--- main.py
+++ main.py
@@ -9,5 +9,22 @@ data_nobel = r.json()
 
 #a)  wypisze ile łącznie osób otrzymało nagrody
 number_prizes = len(data_nobel['laureates'])
-print(number_prizes)
+# print(number_prizes)
+
+#b) wypisz osoby które otrzymały nagrodę w kategorii  ekonomia ‘economy’ , alfabetycznie
+prize_economy = []
+
+for laureate in data_nobel['laureates']:
+    # print(laureate['prizes'])
+    for prize in laureate['prizes']:
+        if prize['category'] == 'economics':
+            prize_economy.append(laureate['firstname'] + ' ' + laureate['surname']) #sortowanie będzie według imienia
+            # prize_economy.append(laureate['surname'] + ' ' + laureate['firstname']) #sortowanie bedzie według nazwiska
+            break
+prize_economy = sorted(prize_economy)
+print(prize_economy)
+
+
+
+
 
