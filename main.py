c3ccd16e99d6d0f44329035ff56f7931d4dd18ce
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2021-03-07 19:46:59 +0100

    ilosc osob z nagrodami

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ URL = 'http://api.nobelprize.org/v1/laureate.json'
 
 r = requests.get(URL)
 data_nobel = r.json()
-print(data_nobel)
+# print(data_nobel)
 
+#a)  wypisze ile łącznie osób otrzymało nagrody
+number_prizes = len(data_nobel['laureates'])
+print(number_prizes)
 
